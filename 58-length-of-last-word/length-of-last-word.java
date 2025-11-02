42a76ed0059ce7a0f09a21e1606ed5f12c9bbf36
Sathkeerthi Y Agnihothri <syagni1947@gmail.com>
2025-11-02 21:50:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (5.02%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,7 +1,19 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         // 1st attempt
-        String[] words = s.trim().split(" ");
-        return words[words.length-1].length();
+        // String[] words = s.trim().split(" ");
+        // return words[words.length-1].length();
+
+        // Better approach
+        int i = s.length()-1;
+        int count = 0;
+        while (i >= 0 && s.charAt(i) == ' ')
+            i--;
+        
+        while (i >= 0 && s.charAt(i) != ' '){
+            count++;
+            i--;
+        }
+        return count;
     }
 }
