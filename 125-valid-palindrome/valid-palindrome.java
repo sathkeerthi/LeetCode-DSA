4bff3c2efb7a76020fb19903bf3ecb60fc174af3
Sathkeerthi Y Agnihothri <syagni1947@gmail.com>
2025-11-03 10:52:04 +0530

    Time: 14 ms (28.41%) | Memory: 46.5 MB (5.89%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,16 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        char[] c = s.toLowerCase().toCharArray();
-        StringBuffer sb = new StringBuffer();
-        for (char i : c){
-            if ((i >= 'a' && i <= 'z') || (i >= '0' && i <= '9')){
-                sb.append(i);
-            }
-        }
-        System.out.println(sb);
+        // char[] c = s.toLowerCase().toCharArray();
+        // StringBuffer sb = new StringBuffer();
+        // for (char i : c){
+        //     if ((i >= 'a' && i <= 'z') || (i >= '0' && i <= '9')){
+        //         sb.append(i);
+        //     }
+        // }
+        // System.out.println(sb);
 
-        for (int i = 0, j = sb.length()-1 ; i <= j ; i++, j--){
-            if (sb.charAt(i) != sb.charAt(j))
+        // for (int i = 0, j = sb.length()-1 ; i <= j ; i++, j--){
+        //     if (sb.charAt(i) != sb.charAt(j))
+        //         return false;
+        // }
+        // return true;
+
+        // Using regex to improve time & space complexity
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        
+        for (int left=0, right=s.length()-1 ; left <= right ; left++, right--){
+            if (s.charAt(left) != s.charAt(right))
                 return false;
         }
         return true;
