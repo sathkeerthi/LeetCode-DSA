ee8098c63d912f55619bf0b7f3346ae3975fc4cd
Sathkeerthi Y Agnihothri <syagni1947@gmail.com>
2025-11-03 22:56:48 +0530

    Time: 4 ms (15.24%) | Memory: 42.5 MB (6.11%) - LeetSync

---
+++ easy/168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        String s = "";
+        while (columnNumber != 0){
+            columnNumber--;
+            int digit = columnNumber % 26;
+            s = (char)(65+digit) + s;
+            System.out.println(s);
+            columnNumber /= 26;
+        }
+        return s;
+    }
+}
